2cdee972bb0de85efdd569b873c2fec4143aeae1
Kerem Hacıfazlıoğlu <cagdas@kobiks.com>
2023-02-16 11:06:57 +0300

    kimlik doğrulama servisleri tanımlandı.

--- lib/repository/impl/auth_repository_impl.dart
+++ lib/repository/impl/auth_repository_impl.dart
@@ -4,11 +4,12 @@ import 'package:platform/domain/request/auth/send_sms_request.dart';
 import 'package:platform/domain/request/auth/token_request.dart';
 import 'package:platform/domain/response/auth/confirm_sms.dart';
 import 'package:platform/domain/response/auth/token.dart';
+import 'package:platform/domain/response/success_response.dart';
 
 abstract class IAuthRepository {
   Future<Token?> token(TokenRequest tokenRequest);
 
-  Future<bool> sendSms(SendSmsRequest sendSmsRequest);
+  Future<SuccessResponse> sendSms(SendSmsRequest sendSmsRequest);
 
   Future<ConfirmSms> confirmSms(ConfirmSmsRequest confirmSmsRequest);
 
